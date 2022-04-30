7235ad560488a8668ebcef73e27dcafbe2a11a14
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2022-04-30 22:47:04 +0200

    SSMA-6 Fetch and list all users from the database to the view.

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./core/Home";
+import Users from "./user/Users";
 
 const MainRouter = () => {
-    return (
-        <div>
-            {/* renders the first child that matches the requested route path. */}
-            <Routes>
-                {/* <Route exact path="/" component={Home} /> */}
-                <Route path="/" exact element={<Home />} />
-            </Routes>
-        </div>
-    );
+	return (
+		<div>
+			{/* renders the first child that matches the requested route path. */}
+			<Routes>
+				{/* <Route exact path="/" component={Home} /> */}
+				<Route path="/" exact element={<Home />} />
+				<Route path="/users" element={<Users />} />
+			</Routes>
+		</div>
+	);
 };
 
 export default MainRouter;
