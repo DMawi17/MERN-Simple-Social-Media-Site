118d9733f702b02761a11fa44c38fa00ca3872c0
DMawi17 <75326045+DMawi17@users.noreply.github.com>
2022-04-30 11:20:15 +0200

    SSMA-3 Add a signout method. Export both methods.

--- client/auth/api-auth.js
+++ client/auth/api-auth.js
@@ -1,3 +1,5 @@
+// SING-IN:
+
 // The component invoking this method needs to handle the response appropriately, such as storing the received JWT locally so it can be used when making calls to other protected API routes from the frontend.
 
 const signin = async (user) => {
@@ -17,3 +19,19 @@ const signin = async (user) => {
         console.log(err);
     }
 };
+
+// SING-OUT
+
+// Use fetch to make a GET call to signout API endpoint on the server.
+const signout = async () => {
+    try {
+        let response = await fetch("/auth/signout", {
+            method: "GET",
+        });
+        return await response.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+export { signin, signout };
